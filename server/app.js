acfb1bc69c0a123cfcc3386e92e3edfbab3183c4
Harrison Elkins <harrisonmelkins@gmail.com>
2021-08-12 16:14:41 -0400

    connect mongoose with express

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express"),
   morgan = require("morgan"),
   app = express();
-
+require("./db/config");
 // Parse incoming JSON into objects
 app.use(express.json());
 // Log all requests
